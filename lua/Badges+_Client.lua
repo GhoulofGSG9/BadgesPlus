f89ff52d418d0a5a18757747dc45809bb4628df4
BrightPaul <sebi4d@gmx.de>
2014-02-02 00:28:04 +0100

    Remove all not needed stuff

--- lua/Badges+_Client.lua
+++ lua/Badges+_Client.lua
@@ -1,5 +1,4 @@
-local function SelectBadge(self, id)
-    
+local function SelectBadge(self, id)    
     for i = 1, #self.dlcIcons do
         local dlcIcon = self.dlcIcons[i]
         if(dlcIcon.id == id) then
@@ -10,42 +9,18 @@ local function SelectBadge(self, id)
     end
 end
 
-local kBadgeServerURL = "http://www.ns2c.com"
-
 originalMenuCreateProfile = Class_ReplaceMethod( "GUIMainMenu", "CreateProfile",
-function(self)    
-    self.profileBackground = CreateMenuElement(self.menuBackground, "Image")
-    self.profileBackground:SetCSSClass("profile")
-
-
-    local eventCallbacks =
-    {
-        // Trigger initial animation
-        OnShow = function(self)
-        
-            // Passing updateChildren == false to prevent updating of children
-            self:SetCSSClass("profile", false)
-            
-        end,
-        
-        // Destroy all animation and reset state
-        OnHide = function(self) end
-    }
+function(self)
+    --first call original method
+    originalMenuCreateProfile(self)
     
-    self.profileBackground:AddEventCallbacks(eventCallbacks)
-    
-    // Create avatar icon.
-    self.avatar = CreateMenuElement(self.profileBackground, "Image")
-    self.avatar:SetCSSClass("avatar")
-    self.avatar:SetBackgroundTexture("*avatar")
-    
-    --Start Mod
+    --now let's do badges+ stuff
     LoadCSSFile("lua/menu/main_menu_badges.css")
     
     self.dlcIcons = {}
     self.badgePos = 0
     
-    // create selectable badges - this only fits 8 badges, profileBackground must be expanded for more to work
+    --create selectable badges - this only fits 8 badges, profileBackground must be expanded for more to work
     local function callback(badges)   
         for i, dlc in ipairs(badges) do
             local dlcIcon = CreateMenuElement(self.profileBackground, "Image")
@@ -108,25 +83,4 @@ function(self)
     end
     
     GetBadgeStrings(callback)
-    --End Mod
-
-    self.playerName = CreateMenuElement(self.profileBackground, "Link")
-    self.playerName:SetCSSClass("profile")
-    
-    self.rankLevel = CreateMenuElement(self.profileBackground, "Link")
-    self.rankLevel:SetCSSClass("rank_level")
-
-    local eventCallbacks =
-    {
-        OnClick = function (self, buttonPressed)
-            Client.ShowWebpage("http://hive.naturalselection2.com/profile/".. Client.GetSteamId())
-		end,
-		
-		OnMouseIn = function (self, buttonPressed)
-            MainMenu_OnMouseIn()
-        end,
-    }
-    
-    self.playerName:AddEventCallbacks(eventCallbacks)
-    self.rankLevel:AddEventCallbacks(eventCallbacks)
 end)
